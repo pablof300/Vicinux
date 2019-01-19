8dc6ca306bb5957f9f5ffbba67f6dbe13f8dbaba
Pablo Estrada <pablof300@gmail.com>
2019-01-19 04:33:57 -0500

    Added a lot of UI to the dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require semantic-ui
-//= require jquery
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).ready('turbolinks:load', function() {
+  $('#cats').click(function() {alert('cats!');});
+  $('#search-select').dropdown();
+  $('.ui.dropdown').dropdown();
+}

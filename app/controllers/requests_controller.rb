39fb2e980521b9429a4691b2f5c81e2fb0a8181c
Dylan <dylanbaker7@gmail.com>
2019-01-19 22:00:46 -0500

    Edited requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,12 @@
 class RequestsController < ApplicationController
+  def new
+
+  end
+  def create
+
+  end
+  def index
+    @category = Category.find_by(id: params[:category_id])
+    @inventories = []  
+  end
 end

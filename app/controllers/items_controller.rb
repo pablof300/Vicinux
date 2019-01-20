16ad3347a3f1e0bd2fe5dd0f972289bc884264b8
Pablo Estrada <pablof300@gmail.com>
2019-01-19 23:46:08 -0500

    Finished items menu

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,27 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    @item.user = current_user
+    @item.date = Date.today
+    if @item.save
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(current_user)
+    else
+      flash[:danger] = "Incorrect information"
+      render :new
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+    redirect_to user_path(current_user)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id)
   end
 end
